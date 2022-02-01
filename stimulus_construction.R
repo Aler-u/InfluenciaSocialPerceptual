d46c2e8f227b16b64ab0c61c9b3b43511b4d38f4
Aler-u <aler.usaj@gmail.com>
2022-02-01 18:19:22 -0300

    Changed the stimulus construction script to add two new proportions resulting in two more new levels of difficulty for the task.

--- stimulus_construction.R
+++ stimulus_construction.R
@@ -1,11 +1,12 @@
-#Matrix size (100x100)
+#Matrix size (100x100) gives the number of individual colored cells in the stimulus
 stim_q <- 100^2
 
 #Colors proportions based on Voss, A., Rothermund, K., & Brandtstädter, J. (2008).
-p <- c(.45,.47,.49)
+#The paper proposes 3 color proportions (45%, 47% and 49%), we add two more levels, 43% and 41% for testing purposes
+p <- c(.41,.43,.45,.47,.49)
 
 shuffled_vectors_fun <- function(x, vector_length = stim_q){
-  #The function creates a vector with length vector_length consisting of a repetition of 1s and 0s with proportions given by the argument
+  #The function creates a vector with length vector_length consisting of a repetition of 1s and 0s with the proportion of 1s given by the argument
   #x, then it shuffles the vector using the sample function
   #Takes two arguments
     ##x = a proportion (from 0 to 1) corresponding to the amount of 1s in the resulting vector
@@ -21,23 +22,23 @@ shuffled_vectors_fun <- function(x, vector_length = stim_q){
 }
 
 ##Training stimulus
-#Create 3 vectors of length 100^2 with shuffled 1s and 0s according to the proportions of object p
+#Create length(p) vectors of length 100^2 with shuffled 1s and 0s according to the proportions of object p
 shuffled_vectors <- sapply(
   p, #Iterate over the three color proportions
   #Function to generate vectors of length 100^2 with 1s and 0s according to each proportion x (from p) and shuffle it
   shuffled_vectors_fun
 )
 
-#Transform the vectors in shuffled_vectors to a 3d array with dimensions 100x100x3
+#Transform the vectors in shuffled_vectors to a 3d array with dimensions 100x100xlength(p), the 100x100 are given by stim_q
 training_stimuli <- array(
   shuffled_vectors,
-  dim = c(100,100,3)
+  dim = c(100,100,length(p))
 )
 ##End Training stimulus
 
 ##Experimental stimulus (Repeat procedure to generate training stimulus with slight variations)
 
-#Create 3000 vectors of length 100^2 with shuffled 1s and 0s, with a different proportion p for each 1000 elements (eg. the first 1000 vectors have proportion 0.45, etc)
+#Create length(p) * 100 vectors of length 100^2 with shuffled 1s and 0s, with a different proportion p for each 100 elements (eg. the first 100 vectors have proportion 0.45, etc)
 experimental_vectors <- sapply(
   rep( #Generate a vector of proportions containing 1000 repeated values for each proportion in p
     p,
@@ -49,7 +50,7 @@ experimental_vectors <- sapply(
 #Transformed the shuffled vectors into a 3d array
 experimental_array <- array(
   experimental_vectors,
-  dim = c(100,100,300)
+  dim = c(100,100,length(p)*100)
 )
 
 #Save the results
